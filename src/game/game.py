db59a3a6668ac6291946f7b02120f3e447722b74
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-09-18 18:22:52 +0800

    Added draw board letters

--- src/game/game.py
+++ src/game/game.py
@@ -1,14 +1,28 @@
-from utils import Colors
+from utils import Colors, LetterFont
 from window import window
 import pygame
+import json
+import os
 
 pygame.init()
+resources_path = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__), 
+        "..", "..", "resources", "game"
+        )
+    )
+
+# Json
+with open(f"{resources_path}/game.json") as json_file:
+    game_data = json.load(json_file)
 
 
-class TicTacToe:
+class TicTacToe(LetterFont):
     display_size_divider = 5
 
     def __init__(self):
+        super().__init__()
+
         wd, ht = window.rect.size
         self.display = pygame.Surface(
             (wd // self.display_size_divider,
@@ -16,6 +30,7 @@ class TicTacToe:
             pygame.SRCALPHA)
         
         self.board = [["" for _ in range(3)] for _ in range(3)]
+        self.letter_positions = game_data["boardletter_positions"]
 
     def draw(self, display):
         # Fill game's display with a transparent background
@@ -28,6 +43,14 @@ class TicTacToe:
                 self.display, Colors.black, (0, size * i), (128, size * i))
             pygame.draw.line(
                 self.display, Colors.black, (size * i, 0), (size * i, 128))
+
+        # Draw letters
+        for i, row in enumerate(self.board):
+            for j, letter in enumerate(row):
+                if letter != "":
+                    pos = self.letter_positions[j][i]
+                    self.render_font(
+                        self.display, letter, pos, enlarge=4)
 
         # Blit game's display to original display
         resized_game_display = pygame.transform.scale(

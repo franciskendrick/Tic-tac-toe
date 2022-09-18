0b2d772621d34b2be44cf6b9422bea442e99396f
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-09-18 17:42:05 +0800

    Fixed game's board lines

--- src/game/game.py
+++ src/game/game.py
@@ -6,30 +6,30 @@ pygame.init()
 
 
 class TicTacToe:
-    # Initialize
-    def __init__(self):
-        self.display = pygame.Surface(
-            window.rect.size, pygame.SRCALPHA)
-        self.board = self.make_board()
+    display_size_divider = 5
 
-    # Draw
+    def __init__(self):
+        wd, ht = window.rect.size
+        self.display = pygame.Surface(
+            (wd // self.display_size_divider,
+            ht // self.display_size_divider),
+            pygame.SRCALPHA)
+        
+        self.board = [["" for _ in range(3)] for _ in range(3)]
+
     def draw(self, display):
         # Fill game's display with a transparent background
         self.display.fill((0, 0, 0, 0))
 
         # Draw lines
         for i in range(1, 3): 
-            size = 640 * 0.333
+            size = 128 * 0.333
             pygame.draw.line(
-                display, Colors.black, (0, size * i), (640, size * i), 5)
+                self.display, Colors.black, (0, size * i), (128, size * i))
             pygame.draw.line(
-                display, Colors.black, (size * i, 0), (size * i, 640), 5)
+                self.display, Colors.black, (size * i, 0), (size * i, 128))
 
         # Blit game's display to original display
         resized_game_display = pygame.transform.scale(
             self.display, display.get_size())
         display.blit(resized_game_display, (0, 0))
-
-    # Functions
-    def make_board(selfs):
-        return [["" for _ in range(3)] for _ in range(3)]
